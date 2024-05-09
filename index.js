2837d486133d1af437782550dedad54af5e45f98
jessica-dds <jessica.gleizer@hotmail.com>
2024-05-08 21:09:37 -0300

    rota Post

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ const app = express();
 
 app.use(express.json());
 
+// app.get('/', async (req, res) => {
+//     res.json(`Olá da api 1. Porta 3000.`);
+
+//     const resultadoAxios = await axios.get('http://localhost:3001/carros'); //listar carros cadastrados na API 2
+
+//     console.log(resultadoAxios);
+
+//     res.json(resultadoAxios.data);
+// });
+
+
 app.get('/', async (req, res) => {
-    //res.json(`Olá da api 1. Porta 3000.`);
 
-    const resultadoAxios = await axios.get('http://localhost:3001/carros');
 
-    console.log(resultadoAxios);
+    const novoCarro = {
+        modelo: 'Camaro',
+        marca: 'Chevrolet'
+    }
+
+    const resultadoAxios = await axios.post('http://localhost:3001/carros', novoCarro); //cadastrar novo carro
 
     res.json(resultadoAxios.data);
 });
